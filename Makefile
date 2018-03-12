d1c3c92bc6aaa22bcebf393f4efda98c39e830e7
Andrew Jacobs <andrew.jacobs@btinternet.com>
2018-03-12 18:00:51 +0000

    Code changes

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all:	bcpl.s28
 
 
 bcpl.s28:	bcpl.obj
-	java -classpath Dev65.jar uk.co.demon.obelisk.w65xx.Lk65 -code "0000-32767" -s28 -output bcpl.s28 *.obj	
+	java -classpath Dev65.jar uk.co.demon.obelisk.w65xx.Lk65 -bss "512-1023" -code "1024-32767" -s28 -output bcpl.s28 *.obj	
 
 bcpl.obj:	bcpl.asm
 	java -classpath Dev65.jar uk.co.demon.obelisk.w65xx.As65 bcpl.asm
--- bcpl.asm
+++ bcpl.asm
@@ -1,24 +1,85 @@
 ;===============================================================================
+;
+; `7MM"""Yp,   .g8"""bgd `7MM"""Mq.`7MMF'      
+;   MM    Yb .dP'     `M   MM   `MM. MM        
+;   MM    dP dM'       `   MM   ,M9  MM        
+;   MM"""bg. MM            MMmmdM9   MM        
+;   MM    `Y MM.           MM        MM      , 
+;   MM    ,9 `Mb.     ,'   MM        MM     ,M 
+; .JMMmmmd9    `"bmmmd'  .JMML.    .JMMmmmmMMM 
+;
+; BCPL for the WDC W65C265SXB
+;-------------------------------------------------------------------------------
+; Copyright (C),2018 Andrew John Jacobs.
+; All rights reserved.
+;
+; This work is licensed under a Creative Commons Attribution NonCommercial-
+; ShareAlike 4.0 International License.
+;
+; See here for details:
+;
+;	https://creativecommons.org/licenses/by-nc-sa/4.0/
+;                                            
 ;-------------------------------------------------------------------------------
 
-
-
 		.65816
+
+;===============================================================================
+; Macros
+;-------------------------------------------------------------------------------
 
 short_a		.macro
 		.longa	off
 		sep	#$20
 		.endm
 
+short_i		.macro
+		.longa	off
+		sep	#$10
+		.endm
+
+short_ai	.macro
+		.longa	off
+		sep	#$30
+		.endm
 
 long_a		.macro
 		.longa	on
 		rep	#$20
 		.endm
 
-MEML		.equ	$c00000
-MEMH		.equ	$c10000
+long_i		.macro
+		.longa	on
+		rep	#$10
+		.endm
 
+long_ai		.macro
+		.longa	on
+		rep	#$30
+		.endm
+
+;===============================================================================
+; Constants
+;-------------------------------------------------------------------------------
+
+; The starting addresses of the data memory area using (/CS7)
+
+MEML		.equ	$c00000		; Lo byte
+MEMH		.equ	$c10000		; Hi byte
+
+; CH376S USB Interface Commands
+
+SET_BAUDRATE	.equ	$02
+ENTER_SLEEP	.equ	$03
+RESET_ALL	.equ	$05
+CHECK_EXIST	.equ	$06
+GET_STATUS	.equ	$22
+
+SET_FILE_NAME	.equ	$aa		; FIX
+
+;===============================================================================
+; Data Areas
+;-------------------------------------------------------------------------------
 
 		.page0
 		.org	$00
@@ -33,12 +94,30 @@ P		.space	2
 W		.space	2
 ;D		.space	2
 
+;-------------------------------------------------------------------------------
+
+		.bss
+		.org	$0200
+
+COMMAND		.space	256
+
+;===============================================================================
+;-------------------------------------------------------------------------------
 
 		.code
 		.org	$0400
 
+; Reset Hardware
+
+; Mount Disk
+
+; Read Command
+
+; Load Target
 
 ;===============================================================================
+; INTCODE Interpreter
+;-------------------------------------------------------------------------------
 
 ;	 15  14  13  12  11  10  9   8   7   6   5   4   3   2   1   0
 ;	+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
@@ -201,13 +280,384 @@ OpcodeK:
 ; Execute Function
 
 OpcodeX:
+		tya			; Get function index
+		if ne			; Not zero?
+		 cmp	#40		; .. but less than 40?
+		 if cc
+		  dec	a		; Scale to jump table index
+		  asl	a
+		  tax
+		  jmp	(Function,x)	; And goto to handler
+		 endif
+		endif
 
-		jmp	(Function,x)
+		bra 	$		; FIX: Invalid function
+
 Function:
-		.word	Function0
-		
-		
-Function0:
+		.word	Function1
+		.word	Function2
+		.word	Function3
+		.word	Function4
+		.word	Function5
+		.word	Function6
+		.word	Function7
+		.word	Function8
+		.word	Function9
+		.word	Function10
+		.word	Function11
+		.word	Function12
+		.word	Function13
+		.word	Function14
+		.word	Function15
+		.word	Function16
+		.word	Function17
+		.word	Function18
+		.word	Function19
+		.word	Function20
+		.word	Function21
+		.word	Function22
+		.word	Function23
+		.word	Function24
+		.word	Function25
+		.word	Function26
+		.word	Function27
+		.word	Function28
+		.word	Function29
+		.word	Function30
+		.word	Function31
+		.word	Function32
+		.word	Function33
+		.word	Function34
+		.word	Function35
+		.word	Function36
+		.word	Function37
+		.word	Function38
+		.word	Function39
+
+;-------------------------------------------------------------------------------
+; A = M[A]
+
+Function1:
+		ldx	ACCA		; Fetch memory address
+		short_a
+		lda	>MEMH,x		; Recover the value
+		xba
+		lda	>MEML,x
+		long_a
+		sta	ACCA		; And store
+		jmp	Step		; Done.
+
+;-------------------------------------------------------------------------------
+; A = -A
+
+Function2:
+		sec
+		lda	#0		; Subtract A from zero
+		sbc	ACCA
+		sta	ACCA		; And store
+		jmp	Step		; Done.
+
+;-------------------------------------------------------------------------------
+; A = ~A
+
+Function3:
+		lda	#$ffff		; Invert all bits in A
+		eor	ACCA
+		sta	ACCA		; And store
+		jmp	Step		; Done.
+
+;-------------------------------------------------------------------------------
+
+Function4:
+Function5:
+Function6:
+Function7:
+
+;-------------------------------------------------------------------------------
+; A = B + A
+
+Function8:
+		clc
+		lda	ACCB
+		adc	ACCA
+		sta	ACCA
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = B - A
+
+Function9:
+		sec
+		lda	ACCB
+		sbc	ACCA
+		sta	ACCA
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = (B == A)
+
+Function10:
+		lda	ACCA
+		stz	ACCA
+		cmp	ACCB
+		if eq
+		 dec	ACCA
+		endif
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = (B != A)
+
+Function11:
+		lda	ACCA
+		stz	ACCA
+		cmp	ACCB
+		if ne
+		 dec	ACCA
+		endif
+		jmp 	Step
+
+;-------------------------------------------------------------------------------
+
+Function12:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function13:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function14:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function15:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = B << A
+
+Function16:
+		lda	ACCB
+		ldx	ACCA
+		if ne
+		 cpx	#$16
+		 if cs
+		  stz	ACCA
+		  jmp	Step
+		 endif
+		 repeat
+		  asl	a
+		  dex
+		 until eq
+		endif
+		sta	ACCA
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = B >> A
+
+Function17:
+		lda	ACCB
+		ldx	ACCA
+		if ne
+		 cpx	#$16
+		 if cs
+		  stz	ACCA
+		  jmp	Step
+		 endif
+		 repeat
+		  lsr	a
+		  dex
+		 until eq
+		endif
+		sta	ACCA
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = B & A
+
+Function18:
+		lda	ACCB
+		and	ACCA
+		sta	ACCA
+		jmp	Step
+	
+;-------------------------------------------------------------------------------
+; A = B | A
+	
+Function19:
+		lda	ACCB
+		ora	ACCA
+		sta	ACCA
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = B ^ A
+
+Function20:
+		lda	ACCB
+		eor	ACCA
+		sta	ACCA
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+; A = ~(B ^ A)
+	
+Function21:
+		lda	ACCB
+		eor	ACCA
+		eor	#$ffff
+		sta	ACCA
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function22:
+		bra	$		; FIX: Exit
+
+;-------------------------------------------------------------------------------
+
+Function23:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function24:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function25:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function26:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function27:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function28:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function29:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function30:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function31:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function32:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function33:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function34:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function35:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function36:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function37:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function38:
+		jmp	Step
+
+;-------------------------------------------------------------------------------
+
+Function39:
+		jmp	Step
+
+;===============================================================================
+; CH376S Module Interface
+;-------------------------------------------------------------------------------
+
+
+		.longa	off
+SetFileName:
+		lda	#SET_FILE_NAME	; Send command
+		jsr	SendCommand
+		repeat
+		 lda	!0,x		; Followed by null terminated 
+		 php			; .. string
+		 jsr	DiskTx
+		 plp
+		until eq
+		jmp	ReadStatus
+
+
+; Transmit the command synchronisation prefix to the CH376 followed by the
+; command code.
+
+		.longa	off
+SendCommand:
+		pha			; Save the command
+		lda	#$57		; Send the prefix
+		jsr	DiskTx		
+		lda	#$aa
+		jsr	DiskTx
+		pla			; Recover command
+		jmp	DiskTx		; And send
+
+;
+		.longa	off
+ReadStatus:
+		rts
+
+;===============================================================================
+; UART Interfaces
+;-------------------------------------------------------------------------------
+
+UartTx:
+		rts
+
+UartRx:
+		rts
+
+;-------------------------------------------------------------------------------
+
+DiskTx:
+		rts
+
+DiskRx:
+		rts
+
 
 
 		.end
